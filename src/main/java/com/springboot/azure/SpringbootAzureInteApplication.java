28a24d9f42fb4e90568103088981628cc1c28e31
kumar367316 <kumarswain367@gmail.com>
2024-02-04 13:42:41 +0530

    add output statement for new deployment

--- src/main/java/com/springboot/azure/SpringbootAzureInteApplication.java
+++ src/main/java/com/springboot/azure/SpringbootAzureInteApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpringbootAzureInteApplication {
 
 	public static void main(String[] args) {
+		System.out.println("New Deployment");
 		SpringApplication.run(SpringbootAzureInteApplication.class, args);
 	}
 
